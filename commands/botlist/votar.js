2ab9473749d6f985d78ff8cc4cfd1fdb32cb0d7d
Takashi-website <takashi.suporte@gmail.com>
2024-10-09 08:05:03 -0300

    Update votar.js

--- commands/botlist/votar.js
+++ commands/botlist/votar.js
@@ -4,6 +4,7 @@ $var[bot;$findUser[$message[2]]]
 $onlyIf[$message[2]!=;( <:errado:1292312941030277200> › <@$authorID>, mencione apenas aplicações para votar.]
 $onlyIf[$isBot[$var[bot]]==true;( <:errado:1292312941030277200> › <@$authorID>, mencione uma aplicação válida.]
 $onlyIf[$textSplit[$getUserVar[bot;$var[bot]];/]$splitText[1]==2;( <:errado:1292312941030277200> › <@$authorID>, essa aplicação anida não foi análisada.]
+$globalCooldown[2h;( <a:Timer:1293529296194768896> › <@$authorID>, você votou recentemente aguarde `%time%` para votar novamente.]
 
 $setUserVar[votos;$sum[$getUserVar[votos;$var[bot]];$if[$textSplit[$getVar[premium;$authorID];/]$splitText[2]==true]2$elseif[$getTimestamp<=$textSplit[$getVar[premium;$authorID];/]$splitText[2]]2$elseif[$getTimestamp>$textSplit[$getVar[premium;$authorID];/]$splitText[2]]1$endif];$var[bot]]
 ( 🎉 › <@$authorID> acaba de votar no incrível bot **$username[$var[bot]]**! 🗳️ Com isso, o bot agora conta com **$getUserVar[votos;$var[bot]] votos**! 🌟 Cada voto é uma demonstração de apoio e carinho para bot. Muito obrigado a todos que participam e fazem parte dessa jornada! Vamos continuar crescendo juntos e levando o **$username[$var[bot]]** a novos patamares! 🚀
